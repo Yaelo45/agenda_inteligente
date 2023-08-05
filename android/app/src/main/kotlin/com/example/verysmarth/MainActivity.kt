a845f696aa759a853cbd0a372f801b64a87dcf67
Yaelo45 <97633094+Yaelo45@users.noreply.github.com>
2023-08-05 17:08:38 -0600

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/verysmarth/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.verysmarth
+
+import io.flutter.embedding.android.FlutterActivity
+import com.dexterous.flutterlocalnotifications.ScheduledNotificationBootReceiver
+
+
+class ScheduledNotificationBootReceiver : ScheduledNotificationBootReceiver()
+class MainActivity: FlutterActivity() {
+}
